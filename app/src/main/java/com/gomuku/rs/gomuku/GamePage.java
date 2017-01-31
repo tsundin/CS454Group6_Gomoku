5ad48d4cb48ee199d4e972704ffd9baf32a6eb47
sfabini <scott.fabini@gmail.com>
2017-01-31 10:59:30 -0800

    add basic 10x10 and 15x15 game board grids

--- app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
+++ app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
@@ -1,7 +1,34 @@
 package com.gomuku.rs.gomuku;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
 /**
  * Created by rs on 1/27/17.
  */
-public class GamePage {
+public class GamePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_page);
+
+        /*
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(GamePage.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
+    }
+
 }
