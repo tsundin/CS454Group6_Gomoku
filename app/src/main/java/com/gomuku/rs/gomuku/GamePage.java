8b96bbb8e28370b9b0a0a7aaafc6010d48e7251b
sfabini <scott.fabini@gmail.com>
2017-01-31 12:32:33 -0800

    delete dead commented code

--- app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
+++ app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
@@ -16,19 +16,6 @@ public class GamePage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_page);
-
-        /*
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(GamePage.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        */
     }
 
 }
