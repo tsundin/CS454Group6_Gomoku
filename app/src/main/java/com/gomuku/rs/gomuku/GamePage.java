d74f6f1f689d91ab515c2742a023bd8b8b47f564
roopas7 <roopasiyer26@gmail.com>
2017-02-14 20:53:01 -0800

    Timer pause and resume and player highlighting

--- app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
+++ app/src/main/java/com/gomuku/rs/gomuku/GamePage.java
@@ -2,6 +2,7 @@ package com.gomuku.rs.gomuku;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,6 +10,9 @@ import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.text.Spannable;
+import android.text.style.BackgroundColorSpan;
 
 /**
  * Created by rs on 1/27/17.
@@ -63,6 +67,8 @@ public class GamePage extends Activity {
         int id = aButton.getId();
         int y_coord = id % board_size;
         int x_coord = id / board_size;
+        FragmentManager fm = getFragmentManager();
+
         if (aButton.getTag() == null) {
             if (player) {
                     aButton.setImageResource(R.drawable.intersection_black_100px_100px);
@@ -71,7 +77,18 @@ public class GamePage extends Activity {
                 Toast.makeText(GamePage.this,
                         "Clicked ID: " + id + " (x,y): (" + x_coord + "," + y_coord + ")",
                         Toast.LENGTH_SHORT).show();
-                }
+
+                com.gomuku.rs.gomuku.GameTimerFragment player1Time = (com.gomuku.rs.gomuku.GameTimerFragment) fm.findFragmentById(R.id.timer);
+                com.gomuku.rs.gomuku.GameTimerFragment player2Time = (com.gomuku.rs.gomuku.GameTimerFragment) fm.findFragmentById(R.id.timer2);
+                player1Time.pause();
+                player2Time.resume();
+                TextView textView = (TextView)findViewById(R.id.player1);
+                textView.setBackgroundColor(0xFFFFCB3D);
+                TextView textView2 = (TextView)findViewById(R.id.player2);
+                textView2.setBackgroundColor(getResources().getColor(R.color.yellow));
+
+
+            }
                 else {
                     aButton.setImageResource(R.drawable.intersection_white_100px_100px);
                     aButton.setTag("White");
@@ -79,7 +96,15 @@ public class GamePage extends Activity {
                 Toast.makeText(GamePage.this,
                         "Clicked ID: " + id + " (x,y): (" + x_coord + "," + y_coord + ")",
                         Toast.LENGTH_SHORT).show();
-                }
+                TextView textView = (TextView)findViewById(R.id.player2);
+                textView.setBackgroundColor(0xFFFFCB3D);
+                TextView textView2 = (TextView)findViewById(R.id.player1);
+                textView2.setBackgroundColor(getResources().getColor(R.color.yellow));
+                com.gomuku.rs.gomuku.GameTimerFragment player1Time = (com.gomuku.rs.gomuku.GameTimerFragment) fm.findFragmentById(R.id.timer);
+                com.gomuku.rs.gomuku.GameTimerFragment player2Time = (com.gomuku.rs.gomuku.GameTimerFragment) fm.findFragmentById(R.id.timer2);
+                player2Time.pause();
+                player1Time.resume();
+            }
             }
         else { // click an already-placed piece, to demo stalemate box
             LinearLayout layout = (LinearLayout) findViewById(R.id.stalemate_and_winner);
--- app/src/main/java/com/gomuku/rs/gomuku/GameTimerFragment.java
+++ app/src/main/java/com/gomuku/rs/gomuku/GameTimerFragment.java
@@ -19,31 +19,57 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class GameTimerFragment extends Fragment {
+    Timer countdown;
+    TextView timerText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.game_timer_fragment, container, false);
-        TextView timerText = (TextView) view.findViewById(R.id.playerTimer);
-        Timer countdown = new Timer(600000, 1000, timerText);
-        countdown.start();
+        timerText = (TextView) view.findViewById(R.id.playerTimer);
+        countdown = new Timer(600000, 1000, timerText);
+        //startTimer();
         return view;
+    }
+
+    public void startTimer() {
+        countdown.start();
+    }
+    public void pause() {
+        countdown.pause();
+    }
+    public void resume() {
+        long pausedTime = countdown.getPausedTime();
+        countdown = new Timer(pausedTime, 1000, timerText);
+        countdown.start();
     }
 
     public class Timer extends CountDownTimer {
         TextView text;
+        long pausedTimeUntilFinished;
+        boolean isPaused;
 
         public Timer(long startTime, long interval, TextView timerText) {
             super(startTime, interval);
             text = timerText;
+            pausedTimeUntilFinished = startTime;
+            isPaused = false;
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
+
+            long millisToDisplay = millisUntilFinished;
+            if (isPaused) {
+                isPaused = false;
+                pausedTimeUntilFinished = millisUntilFinished;
+                cancel();
+                return;
+            }
             String hms= String.format("%02d:%d",
                     TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
                     TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
-                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisToDisplay))
             );
-            text.setText("Time remaining:\n\n" + hms);
+            text.setText("Time : " + hms);
             //text.setText("Time remaining:\n\n" + millisUntilFinished / 1000);
         }
 
@@ -51,5 +77,13 @@ public class GameTimerFragment extends Fragment {
         public void onFinish() {
             text.setText("Out of time!");
         }
+
+        public void pause() {
+            isPaused = true;
+        }
+        public long getPausedTime() {
+            return pausedTimeUntilFinished;
+        }
+
     }
 }
